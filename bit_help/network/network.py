3d73be7574287423efaf2e64433371ba6ed859fa
daveusa31 <bakteev2003@yandex.ru>
2020-08-13 22:30:26 +0300

    Added ability send money

--- bit_help/network/network.py
+++ bit_help/network/network.py
@@ -1,6 +1,8 @@
 import bit
 import random
 import bitcoin
+import requests
+import bitcoinfees 
 import blockcypher
 
 
@@ -10,6 +12,12 @@ from bit_help.network import types
 
 
 class Network(types.Address):
+    __AVIABLE_FEES = {
+        "min": "fastestFee", 
+        "average": "halfHourFee", 
+        "max": "hourFee",
+    }
+    
     def __init__(self, address=None, public_key=None, private_key=None):
         super().__init__(address, public_key, private_key)
         self.bit_key = bit.PrivateKey(super().wif)
@@ -20,15 +28,21 @@ class Network(types.Address):
         address = bitcoin.pubtoaddr(public_key)
 
         response = types.Address(address, public_key, private_key)
+        super().__init__(address, public_key, private_key)
 
         return response
 
     def transaction_history(self):
         return bitcoin.history(self.address)
 
-    def send_money(self, address, sum):
-        # mktx
-        pass
+    def send_money(self, address, sum, fee=None, speed=None):
+        """
+        Speed min, average, max
+        """
+        fee = self.__commission_calculate(fee, speed)
+        output = [(address, sum, "btc")]
+        response = self.bit_key.send(output, fee=fee)
+        return response
 
     def balance(self):
         service = random.choice(["blockcypher","bit",])
@@ -36,9 +50,27 @@ class Network(types.Address):
         if "blockcypher" == service:
             balance_in_satoshis = blockcypher.get_total_balance(self.address)
         elif "bit" == service:
-            balance_in_satoshis = self.bit_key.sget_balance()
+            balance_in_satoshis = self.bit_key.get_balance()
             
         balance_in_bitcoins = utilits.convert_satoshis_to_bitcoins(balance_in_satoshis)
         return balance_in_bitcoins
+
+    
+    def __commission_calculate(self, fee, speed):
+        if fee or speed:
+            if 0 < fee:
+                response = fee
+            elif speed:
+                if speed in self.__AVIABLE_FEES: 
+                    response = bitcoinfees.recommended()[self.__AVIABLE_FEES[speed]]
+                else:
+                    exception_text = "Invalid speed. Aviable {}"
+                    raise ValueError(exception_text.format(self.__AVIABLE_FEES))
+        else:
+            response = bitcoinfees.recommended()[self.__AVIABLE_FEES["average"]]
+        
+        return response
+                
+        
 
 
