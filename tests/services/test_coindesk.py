90d7c56cbfdeb527257956a4384e03c488a014b9
daveusa31 <bakteev2003@yandex.ru>
2020-11-17 19:45:26 +0300

    Rewrite coindesk unit test on class

--- tests/services/test_coindesk.py
+++ tests/services/test_coindesk.py
@@ -1,20 +1,21 @@
 import os
 import sys
 import random
+# noinspection PyPackageRequirements
 import pytest
 
 import bit_help
 
-coindesk = bit_help.services.Coindesk()
 
+class TestCoindesk:
+    def setup(self):
+        self.coindesk = bit_help.services.Coindesk()
 
-def test_price():
-    assert isinstance(coindesk.price(), float)
+    def test_price(self):
+        assert isinstance(self.coindesk.price(), float)
 
+    def test_historical_data(self):
+        assert isinstance(self.coindesk.historical_data(), dict)
 
-def test_historical_data():
-    assert isinstance(coindesk.historical_data(), dict)
-
-
-def test_supported_currencies():
-    assert isinstance(coindesk.supported_currencies(), list)
+    def test_supported_currencies(self):
+        assert isinstance(self.coindesk.supported_currencies(), list)
